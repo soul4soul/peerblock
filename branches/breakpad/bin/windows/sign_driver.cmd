b482eb19f600a7501b7bc203f748d39aaac96165
nightstalkerz <nightstalkerz@d3e0e2f6-51e0-11de-b413-c5e99dc0a8e2>
2011-05-30 21:26:45 +0000

    Remove breakpad branch.

--- branches/breakpad/bin/windows/sign_driver.cmd
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-"%PB_DDK_DIR%\bin\SelfSign\signtool.exe" sign /v /ac %PB_CERT% /s my^
- /n "PeerBlock, LLC" /t http://timestamp.globalsign.com/scripts/timstamp.dll %1
-
-ECHO. & ECHO.
-ECHO Done!
-ECHO.
